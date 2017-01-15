523d5e6424cdd5b2a7c234a823c300ada881775b
Tommy Ip <hkmp7tommy@gmail.com>
2017-01-15 14:42:08 +0000

    code: Create main interface for tabled.tableD.

--- tabled/tabled.py
+++ tabled/tabled.py
@@ -1,7 +1,67 @@
-def hello_world() -> None:
-    """ First function of tabled!
+"""
+tableD: Table for Humans?
 
-    >>> hello_world()
-    Hello, World!
+Author: Tommy Ip <hkmp7tommy@gmail.com>
+License: MIT
+Github repository: https://github.com/tommyip/tabled
+Python version: 3.5
+
+This is the main python module for tableD. Please read our documentation for
+detailed usage information and developer guide.
+"""
+
+from typing import List, Any, Text
+
+
+class TableD:
     """
-    print("Hello, World!")
+    This is the public interface for TableD.
+    """
+    def __init__(self,
+                 headings: List[Text] = [],
+                 data: List[List[Any]] = [],
+                 style: Text = 'default') -> None:
+        """
+        Initialize data storage engine for TableD. Column headings should
+        be a valid lists of unicode strings while individual cells can
+        contain any values. The data argument should have the same ordering
+        as its headings.
+
+        >>> TableD(
+        ...     ['Repo', 'Author', 'Url'],
+        ...     [['tableD', 'Tommy Ip', 'http://github.com/tommyip/tabled'],
+        ...      ['Exmaple', 'Someone', 'http://example.com']],
+        ...     'fancy'
+        ... )
+        <tabled.tabled.TableD object at 0x...>
+        """
+
+        self.headings = headings
+        self.data = data
+        self.style = style
+
+    def add_row(self, row: List[Any] = []) -> None:
+        """
+        Append a single row to instance table. If no argument is passed in, a
+        blank row would be inserted.
+
+        >>> table = TableD()
+        >>> table.add_row(['x1', 'x2', 'x3'])
+        """
+        pass
+
+    def add_rows(self, rows: List[List[Any]]) -> None:
+        """
+        Append multiple row to instance table.
+
+        >>> table = TableD()
+        >>> table.add_rows([['x1', 'x2', 'x3'],
+        ...                 ['y1', 'y2', 'y3']])
+        """
+        pass
+
+    def show(self) -> None:
+        """
+        Pretty print data in table format.
+        """
+        pass
--- tests/test_tabled.py
+++ tests/test_tabled.py
@@ -1,3 +1,38 @@
-def test_hello_world() -> None:
-    awesomeness = 9000  # type: int
-    assert awesomeness == 9000
+"""
+tableD: Table for Humans?
+
+Author: Tommy Ip <hkmp7tommy@gmail.com>
+License: MIT
+Github repository: https://github.com/tommyip/tabled
+Python version: 3.5
+
+This is the main unittest module for tableD. We use pytest as our testing
+framework, visit http://doc.pytest.org/en/latest/ for more information.
+
+To run the test suite, execute the following in the project's root:
+
+    $ py.test
+
+Both unittest and doctest will be invoked.
+"""
+
+from tabled import tabled
+
+
+class TestTableD:
+    def test_create_new_table_blank(self) -> None:
+        table = tabled.TableD()
+
+        assert table.headings == []
+        assert table.data == []
+        assert table.style == 'default'
+
+    def test_create_new_table(self) -> None:
+        headings = ['Name', 'Age', 'Email', 'Url']
+        data = [['Tommy Ip', 17, 'hkmp7tommy@gmail.com', 'example.com'],
+                ['Someone', 23, 'someone@example.com', 'me.example.net']]
+        table = tabled.TableD(headings, data, 'fancy')
+
+        assert table.headings == headings
+        assert table.data == data
+        assert table.style == 'fancy'
