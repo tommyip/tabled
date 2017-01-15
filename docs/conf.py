4a69f716002fb5bd7884f40c8b4104909c3c6acb
Tommy Ip <hkmp7tommy@gmail.com>
2017-01-15 16:50:35 +0000

    docs: Add HTML sidebar to documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -81,6 +81,7 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
+html_sidebars = {'**': ['sidebar.html', 'globaltoc.html', 'searchbox.html']}
 
 # -- Options for HTML output ----------------------------------------------
 
