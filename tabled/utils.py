54a22f69d17ffe0fdacd7df86011c169ebd521b9
Tommy Ip <hkmp7tommy@gmail.com>
2017-01-18 21:46:36 +0000

    code: Add utils utils.rotate_table().

--- tabled/utils.py
+++ tabled/utils.py
@@ -26,3 +26,32 @@ def max_width(column: List[Text]) -> int:
 
     longest_string = max(column, key=len)
     return len(longest_string)
+
+
+def rotate_table(table: List[List[Text]]) -> List[List[Text]]:
+    """ Rotate table so that each row contains element from the same column.
+
+    Rotate a nested list such that the columns become rows, allowing the
+    maximum length of each column to be calcuated.
+
+    Args:
+        table: Nested list of lists that is represented as a table structure.
+
+    Returns:
+        A rotated table with columns as rows.
+
+    Note:
+        This operation is non directional, so applying this function twice
+        would return the orginal input.
+
+    Example:
+        >>> rotate_table([['x1', 'x2', 'x3'],
+        ...               ['y1', 'y2', 'y3'],
+        ...               ['z1', 'z2', 'z3']])
+        ... # doctest: +NORMALIZE_WHITESPACE
+        [['x1', 'y1', 'z1'],
+         ['x2', 'y2', 'z2'],
+         ['x3', 'y3', 'z3']]
+    """
+
+    return [list(row) for row in zip(*table)]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,7 +7,7 @@ tests.pretty_print
 :license: MIT
 """
 
-from tabled.utils import max_width
+from tabled.utils import max_width, rotate_table
 
 
 class TestMaxWidth:
@@ -19,3 +19,39 @@ class TestMaxWidth:
 
     def test_single_element(self) -> None:
         assert max_width(['Wow this is long']) == 16
+
+
+class TestRotateTable:
+    table = [['a1', 'b1', 'c1'],
+             ['a2', 'b2', 'c2'],
+             ['a3', 'b3', 'c3']]
+
+    def test_normal(self) -> None:
+        expected = [['a1', 'a2', 'a3'],
+                    ['b1', 'b2', 'b3'],
+                    ['c1', 'c2', 'c3']]
+
+        assert rotate_table(TestRotateTable.table) == expected
+
+    def test_reversible(self) -> None:
+        rotate_twice = rotate_table(rotate_table(TestRotateTable.table))
+
+        assert rotate_twice == TestRotateTable.table
+
+    def test_asymmetric_list(self) -> None:
+        asymmetric_table = [['a', 'b', 'c', 'd', 'e'],
+                            ['1', '2', '3', '4', '5']]
+
+        expected = [['a', '1'],
+                    ['b', '2'],
+                    ['c', '3'],
+                    ['d', '4'],
+                    ['e', '5']]
+
+        assert rotate_table(asymmetric_table) == expected
+
+    def test_single_column(self) -> None:
+        column = [['a'], ['b'], ['c'], ['d']]
+        row = [['a', 'b', 'c', 'd']]
+
+        assert rotate_table(column) == row
