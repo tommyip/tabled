95b45322f221c5105e56688ecead6405a60d06a2
Tommy Ip <hkmp7tommy@gmail.com>
2017-01-18 14:15:31 +0000

    code: Added utils pretty_print.left_right_pad().

--- tabled/pretty_print.py
+++ tabled/pretty_print.py
@@ -14,8 +14,8 @@ def left_pad(string: Text, width: int) -> Text:
     """ Left pad a string in a container.
 
     Args:
-        string: A text value to be padded with space.
-        width: The width of the string container
+        string: A text value to be padded with spaces.
+        width: The width of the string container.
 
     Returns:
         A string `width` wide with `string` aligned right.
@@ -38,8 +38,8 @@ def right_pad(string: Text, width: int) -> Text:
     """ Right pad a string in a container.
 
     Args:
-        string: A text value to be padded with space.
-        width: The width of the string container
+        string: A text value to be padded with spaces.
+        width: The width of the string container.
 
     Returns:
         A string `width` wide with `string` aligned left.
@@ -56,3 +56,33 @@ def right_pad(string: Text, width: int) -> Text:
         raise ValueError('The input string is longer than the allowed width.')
 
     return "{}{}".format(string, " " * pad_amount)
+
+
+def left_right_pad(string: Text, width: int) -> Text:
+    """ Insert padding in both side of a string inside a container.
+
+    Note:
+        If the size of the padding cannot be divided equally by 2, the right
+        side would be allocated with more spaces.
+
+    Args:
+        string: A text value to be padded with spaces.
+        width: The width of the string container.
+
+    Returns:
+        A string `width` wide with `string` aligned center.
+
+    Example:
+        >>> left_right_pad('tableD', 11)
+        '  tableD   '
+    """
+
+    string_length = len(string)
+    pad_amount = width - string_length
+
+    if pad_amount < 0:
+        # Container too narrow
+        raise ValueError('The input string is longer than the allowed width.')
+
+    left_padded = left_pad(string, (pad_amount // 2) + string_length)
+    return right_pad(left_padded, width)
--- tests/test_pretty_print.py
+++ tests/test_pretty_print.py
@@ -9,7 +9,7 @@ tests.pretty_print
 
 import pytest
 
-from tabled.pretty_print import left_pad, right_pad
+from tabled.pretty_print import left_pad, right_pad, left_right_pad
 
 
 class TestLeftPad:
@@ -23,6 +23,7 @@ class TestLeftPad:
         with pytest.raises(ValueError):
             left_pad('Very long line of text', 5)
 
+
 class TestRightPad:
     def test_normal(self) -> None:
         assert right_pad('Python', 13) == 'Python       '
@@ -33,3 +34,15 @@ class TestRightPad:
     def test_too_narrow(self) -> None:
         with pytest.raises(ValueError):
             right_pad('Very long line of text', 5)
+
+
+class TestLeftRightPad:
+    def test_normal(self) -> None:
+        assert left_right_pad('test', 8) == '  test  '
+
+    def test_uneven_pad_space(self) -> None:
+        assert left_right_pad('example text', 15) == ' example text  '
+
+    def test_too_narrow(self) -> None:
+        with pytest.raises(ValueError):
+            left_right_pad('long string', 2)
