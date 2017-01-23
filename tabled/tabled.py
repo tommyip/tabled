9005246b80c0534b00a6e00b06d5b84675b5dfce
Tommy Ip <hkmp7tommy@gmail.com>
2017-01-23 17:52:38 +0000

    docs: Move TableD class attribute to class string.

--- tabled/tabled.py
+++ tabled/tabled.py
@@ -15,30 +15,29 @@ from .pretty_print import generate_table
 
 
 class TableD:
-    """ tableD's main interface. """
+    """ tableD's main interface.
+
+    Attributes:
+        headings: Valid list of unicode strings for column headings.
+        data: Nested list of cell data.
+        style: Visual styling of the output table.
+        device: Where should the output be presented.
+
+    Example:
+        >>> TableD(
+        ...     ['Repo', 'Author', 'Url'],
+        ...     [['tableD', 'Me', 'http://github.com/tommyip/tabled'],
+        ...      ['Exmaple', 'Someone', 'http://example.com']]
+        ... )
+        <tabled.tabled.TableD object at 0x...>
+    """
 
     def __init__(self,
                  headings: Optional[List[Text]] = None,
                  data: Optional[List[List[Any]]] = None,
                  style: Text = 'default',
                  device: Text = 'stdout') -> None:
-        """ Initialize data storage engine for TableD.
-
-        Args:
-            headings: Valid list of unicode strings for column headings.
-            data: Nested list of cell data.
-            style: Visual styling of the output table.
-            device: Where should the output be presented.
-
-        Example:
-            >>> TableD(
-            ...     ['Repo', 'Author', 'Url'],
-            ...     [['tableD', 'Me', 'http://github.com/tommyip/tabled'],
-            ...      ['Exmaple', 'Someone', 'http://example.com']],
-            ...     'fancy'
-            ... )
-            <tabled.tabled.TableD object at 0x...>
-        """
+        """ Initialize data storage engine for TableD. """
 
         # Python gotcha: mutable default argument
         self.headings = headings or []
@@ -62,7 +61,7 @@ class TableD:
         self.data.append(row)
 
     def add_rows(self, rows: List[List[Any]]) -> None:
-        """ Append multiple rows to table.
+        """ Append multiple rows to data.
 
         Args:
             rows: Multiple rows of data to be appended to table.
@@ -90,4 +89,5 @@ class TableD:
     def _data_2_str(self) -> None:
         """ Convert all fields of the table to string type. """
 
+        self.headings = [str(heading) for heading in self.headings]
         self.data = [[str(cell) for cell in row] for row in self.data]
