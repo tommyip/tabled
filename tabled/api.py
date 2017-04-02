82eff3d2006e529de2ba896dd6fff16cc6795da7
Nick Hale <nickhale@protonmail.ch>
2017-04-02 11:39:40 +0100

    Corrected commit

--- tabled/api.py
+++ tabled/api.py
@@ -35,31 +35,4 @@ def new(headings: Optional[List[Any]] = None,
         >>> new(dataframe=new_df)
     """
 
-<<<<<<< HEAD
     return TableD(headings or [], data or [], style, device, dataframe)
-=======
-    return TableD(headings or [], data or [], style, device)
-
-
-def new_from_df(dataframe,
-                style: Text = 'default',
-                device: Text = 'stdout') -> TableD:
-
-    """ Creates a new TableD object. This should be used instead of calling
-    TableD's __init__() directly.
-
-    Args:
-        Pandas dataframe object.
-
-    Returns:
-        A TableD object.
-
-    Example:
-        >>> new()
-        <tabled.tabled.TableD object at 0x...>
-    """
-    headings = list(dataframe.columns)
-    data = dataframe.values.tolist()
-
-    return TableD(headings, data, style, device)
->>>>>>> 86f9994ab00c49cca1249e46da59125ffa56dfd1
